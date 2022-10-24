308eeb9861b0bad2115146c4bab684d8a6e3ffb0
MrIyo <satriyogithub@gmail.com>
2022-10-24 11:37:37 +0700

    Commit Pertama

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+@extends('template')
+@section('content')
+
+<div class="jumbotron">
+    <h1 class="display-4">Hello, world!</h1>
+    <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+    <hr class="my-4">
+    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+    <a class="btn btn-danger btn-lg" href="{{ url('login/google') }}" role="button">Login via Google</a>
+  </div>
+
+@endsection
